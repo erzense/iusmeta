0938bf278bc2c0b5554b075ff46bf75190483b88
erzense <erayersan21@gmail.com>
2023-09-13 20:11:00 +0300

    u

--- router/app-router.js
+++ router/app-router.js
@@ -7,6 +7,9 @@ getAdminDashboard, deletePostFromAdminDashboard,
 } = require("../controllers/app-controller")
 
 router.route("/").get(getIndex)
+router.get("/ads.txt").get(function(req,res) {
+    res.send("../")
+})
 
 router.route("/about").get(getAbout)
 router.route("/about/:id").get(getAdmin)
